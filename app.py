5a687fe3f12e2301c33a8edf3125f997d4521d8b
Anujgour22 <Anujgour22@iitk.ac.in>
2026-09-07 11:03:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 import joblib
@@ -26,46 +25,94 @@ st.write(
 st.subheader("Customer Information")
 
 Age = st.number_input("Age", min_value=18, max_value=100, value=30)
+
 TypeofContact = st.selectbox(
-    "Type of Contact", ["Self Enquiry", "Company Invited"]
+    "Type of Contact",
+    ["Self Enquiry", "Company Invited"]
 )
+
 CityTier = st.selectbox("City Tier", [1, 2, 3])
+
 DurationOfPitch = st.number_input(
-    "Duration of Pitch", min_value=0, value=10
+    "Duration of Pitch",
+    min_value=0,
+    value=10
 )
+
 Occupation = st.selectbox(
     "Occupation",
     ["Salaried", "Small Business", "Large Business", "Free Lancer"]
 )
-Gender = st.selectbox("Gender", ["Male", "Female"])
+
+Gender = st.selectbox(
+    "Gender",
+    ["Male", "Female"]
+)
+
 NumberOfPersonVisiting = st.number_input(
-    "Number of Persons Visiting", min_value=1, value=2
+    "Number of Persons Visiting",
+    min_value=1,
+    value=2
 )
+
 NumberOfFollowups = st.number_input(
-    "Number of Followups", min_value=0, value=1
+    "Number of Followups",
+    min_value=0,
+    value=1
 )
+
 ProductPitched = st.selectbox(
     "Product Pitched",
     ["Basic", "Standard", "Deluxe", "Super Deluxe", "King"]
 )
+
 PreferredPropertyStar = st.number_input(
-    "Preferred Property Star", min_value=1, max_value=5, value=3
+    "Preferred Property Star",
+    min_value=1,
+    max_value=5,
+    value=3
 )
+
 MaritalStatus = st.selectbox(
     "Marital Status",
     ["Married", "Single", "Divorced", "Unmarried"]
 )
+
 NumberOfTrips = st.number_input(
-    "Number of Trips", min_value=0, value=1
+    "Number of Trips",
+    min_value=0,
+    value=1
 )
-Passport = st.selectbox("Passport", [0, 1])
+
+Passport = st.selectbox(
+    "Passport",
+    [0, 1]
+)
+
 PitchSatisfactionScore = st.number_input(
-    "Pitch Satisfaction Score", min_value=1, max_value=5, value=3
+    "Pitch Satisfaction Score",
+    min_value=1,
+    max_value=5,
+    value=3
 )
-OwnCar = st.selectbox("Own Car", [0, 1])
+
+OwnCar = st.selectbox(
+    "Own Car",
+    [0, 1]
+)
+
 NumberOfChildrenVisiting = st.number_input(
-    "Number of Children Visiting", min_value=0, value=0
+    "Number of Children Visiting",
+    min_value=0,
+    value=0
 )
+
+MonthlyIncome = st.number_input(
+    "Monthly Income",
+    min_value=0,
+    value=25000
+)
+
 Designation = st.selectbox(
     "Designation",
     ["Manager", "Executive", "Senior Manager", "AVP", "VP"]
@@ -90,6 +137,7 @@ if st.button("Predict"):
         "PitchSatisfactionScore": PitchSatisfactionScore,
         "OwnCar": OwnCar,
         "NumberOfChildrenVisiting": NumberOfChildrenVisiting,
+        "MonthlyIncome": MonthlyIncome,
         "Designation": Designation
     }])
 
